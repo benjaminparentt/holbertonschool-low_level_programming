f5f7f375b4f0175d55857845638ec0591542ce5f
benjaminparentt <p.benjamin620@icloud.com>
2023-10-17 01:55:35 -0700

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,14 +7,9 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
 	_putchar('n');
 	_putchar('\n');
 
