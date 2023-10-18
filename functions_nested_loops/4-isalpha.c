0d6ce8502b344b61f89831dadf552fef6f9ed3b9
benjaminparentt <p.benjamin620@icloud.com>
2023-10-18 02:23:30 -0700

    Write a function that checks for alphabetic character.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
 #include "main.h"
-int _isalpha(int C)
+/**
+ * isalpha - Checks for alphabetic character
+ * @c: the character to be checked
+ * 
+ * Retur: 1 for alphabetic character or 0 for anything else
+ */
+ int _isalpha(int C)
 {
 	if ((c<=90 && c>=65)) | ((c>=97 && c<=122))
 	{
