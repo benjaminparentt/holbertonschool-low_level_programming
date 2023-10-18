6a6afcfbabc3b4a45ac657dc03c0e19e72552434
benjaminparentt <p.benjamin620@icloud.com>
2023-10-18 00:15:58 -0700

    print alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,3 @@
-/*
- * File: 2-print_alphabet.c
- * Auth: Brennan D Baraban
- */
-
 #include <main.h>
 
 /**
@@ -14,7 +9,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <=10 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
