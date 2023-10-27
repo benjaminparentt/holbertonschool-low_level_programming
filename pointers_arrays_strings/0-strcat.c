fcda540f7d948b586d7b1780955faae9b8343b0d
benjaminparentt <p.benjamin620@icloud.com>
2023-10-27 00:35:54 -0700

    write a function concatenates two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src);
+{
+	int v;
+	int b;
+	
+	b=0;
+	while (dest[b] != '\0')
+	{
+		k++;
+	}
+	v=0;
+	while (src[v] != '\0')
+	{
+		dest[b] = src[n];
+		b++;
+		v++;
+	}
+	dest[k] = '\0';
+	return dest;
+}
