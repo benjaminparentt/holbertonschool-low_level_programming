b3279a3026dfb11c5c1512ebb7ef63b1fdd0b463
benjaminparentt <p.benjamin620@icloud.com>
2023-10-28 09:29:48 -0700

    Write a function that copies a string

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,10 @@
+include "main.h"
+
+/**
+ *  * _strcat - concatenate two strings
+ *   * @dest: char string to concatenate to
+ *    * @src: char string
+ *     * Return: pointer to resulting string `dest`
+ *      */
+
+char *_strcat(char *dest, char *src)
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *  * _strcat - concatenate two strings
+ *   * @dest: char string to concatenate to
+ *    * @src: char string
+ *     * Return: pointer to resulting string `dest`
+ *      */
+
+
+char* copier(char *dest, const char *src, int n) {
+	for (int i = 0; i < n; i++) {
+		dest[i] = src[i];
+		// if (src[i] == '\0') // Si la fin de la source est atteinte, on s'arrête
+		//         //     break;
+	}
+}
+int main(void)
+{
+	char s1[98];
+	char *ptr;
+	int i;
+	for (i = 0; i < 98 - 1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
+	ptr = copier(s1, "Voici la phrase que je veux copier", 20);
+	printf("%s", s1);
+
+	return (0);
+
