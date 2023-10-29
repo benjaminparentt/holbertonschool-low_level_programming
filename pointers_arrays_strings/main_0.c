9ec35c5dd1810d3097c7c4ff74055bafa58e807f
benjaminparentt <p.benjamin620@icloud.com>
2023-10-29 10:05:58 -0700

    Write a function that concatenates two strings.

---
+++ pointers_arrays_strings/main_0.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+char *_strncpy(char *, char *, int);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98];
+	char *p;
+	int i;
+
+	for (i = 0; i < 98 - 1; i++)
+		s1[i] = '-';
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	p = _strncpy(s1, "Talk is cheap. Show me the code.\n", 14);
+	printf("%s\n", s1);
+	printf("%s\n", p);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+			printf(" ");
+		if (!(i % 10) && i)
+			printf("\n");
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+	return (0);
+}
