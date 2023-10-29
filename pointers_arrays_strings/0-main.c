7c1afbf971bbc33dec5c1e0e41a599f503410da6
benjaminparentt <p.benjamin620@icloud.com>
2023-10-29 10:00:42 -0700

    Write a function that concatenates two strings.

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,18 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - check the code 
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int n;
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *p;
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
+	printf("%s\n", s1);
+	printf("%s", s2);
+	p = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", p);
+	return (0);
 }
