e990b48e0537af9b5083f0984d5b3eaaa9318978
benjaminparentt <p.benjamin620@icloud.com>
2023-10-29 09:59:08 -0700

    Write a function that concatenates two strings.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOBLERTON_H
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -9,3 +11,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+#endif
+
