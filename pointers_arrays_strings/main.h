f6fd71a74a560d3d3694d07c7633272d1ae35469
benjaminparentt <p.benjamin620@icloud.com>
2023-10-29 10:20:05 -0700

    test

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,7 +12,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -23,5 +22,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-
